65364ad40eb7b85efbbd7f82f9172065678cea57
shiva <44841752+sivasamym@users.noreply.github.com>
2023-01-06 00:16:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 
 import streamlit
 streamlit.title("My personal Healthy Diner")
-streamlit.title("poda poda punnakku")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
